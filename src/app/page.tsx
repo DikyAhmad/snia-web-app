b1e953387c54edc754e500af5031c5431dfaf54f
DikyAhmad <diky.ahmad28@gmail.com>
2023-10-15 16:06:10 +0700

    update 2.7.1 fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-8">
       {/* <FormEmail /> */}
       <p className="text-xl font-['Oswald'] my-8 text-center mx-auto">SNIA PHOTO APP</p>
-      <button href="/email"className="btn btn-outline btn-success text-center w-full max-w-xs my-4"><Link className="w-full max-w-xs" href="/email">Email</Link></button>
-      <button href="/email" className="btn btn-outline btn-warning text-center w-full max-w-xs my-4"><Link className="w-full max-w-xs" href="/nota">Nota</Link></button>
+      <button href="/email"className="btn btn-outline btn-success text-center w-full max-w-xs my-4"><Link className="w-full" href="/email">Email</Link></button>
+      <button href="/email" className="btn btn-outline btn-warning text-center w-full max-w-xs my-4"><Link className="w-full" href="/nota">Nota</Link></button>
       <Analytics />
     </main>
   )
