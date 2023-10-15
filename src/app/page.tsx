b256ec1c60a5a8509f7d20d64f3ed738a1eb8a37
DikyAhmad <diky.ahmad28@gmail.com>
2023-10-15 11:23:45 +0700

    update 1.3 fix 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import React, { useState } from 'react';
 export default function Home() {
   const [email, setEmail] = useState("");
   const [service, setService] = useState("");
-  // const [file, setFile] = useState("");
   const [body, setBody] = useState();
 
   const serviceValue = [
@@ -19,8 +18,7 @@ export default function Home() {
 
   function checkService(props: any){
     if (props == "Foto Studio") {
-      // setBody("Pelanggan yang terhormat, \nTerimakasih telah menggunakan layanan kami, berikut merupakan hasil foto studio dalam bentuk softcopy\n\n\nSNIA Photo Studio")
-      setBody(fotos())
+      setBody("Pelanggan yang terhormat,\nTerimakasih telah menggunakan layanan kami, berikut merupakan hasil foto studio dalam bentuk softcopy\n\n\nSNIA Photo Studio")
     } else {
       setBody("UWU")
     }
@@ -33,20 +31,6 @@ export default function Home() {
 
   function mailTo(){
     window.location.href = "mailto:"+email+"?subject="+service+"&body="+body;
-  }
-
-  function fotos(){
-    return(
-      <main>
-        <p>Pelanggan yang terhormat,</p>
-        <p>Terimakasih telah menggunakan layanan kami, berikut merupakan hasil foto studio dalam bentuk softcopy</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>SNIA Photo Studio</p>
-      </main>
-    )
   }
 
   function getEmail(){
