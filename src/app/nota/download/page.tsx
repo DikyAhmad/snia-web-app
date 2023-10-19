fa410f694f19663a11d7049152edc75547803de1
DikyAhmad <diky.ahmad28@gmail.com>
2023-10-19 14:34:36 +0700

    update 4.1.2

--- src/app/nota/download/page.tsx
+++ src/app/nota/download/page.tsx
@@ -2,10 +2,8 @@
 import { PDFDownloadLink} from '@react-pdf/renderer/lib/react-pdf.browser.cjs.js';
 import PdfGenerator from '../PdfGenerator'
 import { Button } from '@mui/material'
-import dynamic from "next/dynamic";
 
-export default function Page({searchParams}){
-    const PDFGenerator = dynamic(() => import('../PdfGenerator'), {ssr: false})
+export default function Page({searchParams}:{searchParams:any}){
     return(
         <main className="flex min-h-screen items-center flex-col p-8">
             <PDFDownloadLink document={<PdfGenerator datas={JSON.parse(searchParams.search)} />} fileName="notapembayaran.pdf">
