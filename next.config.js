601aecd0adcd0d6439b435433aced8070930f1fc
DikyAhmad <diky.ahmad28@gmail.com>
2023-10-21 10:57:25 +0700

    update 4.7.1

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
+    register: true,
+    skipWaiting: true,
     swSrc: 'service-worker.js',
   },
 });
