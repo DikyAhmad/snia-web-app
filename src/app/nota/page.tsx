46fbabb09ac2b7bb08db27f11499c431bc582171
DikyAhmad <diky.ahmad28@gmail.com>
2023-10-22 00:41:32 +0700

    update 5.5 disable ssr rendering

--- src/app/nota/page.tsx
+++ src/app/nota/page.tsx
@@ -6,6 +6,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 export default function NotaPage() {
 
     const DynamicNota = dynamic(() => import('./components/NotaForm'), {
+        ssr: false,
         loading: () => 
         <main className="flex min-h-screen flex-col items-center pt-24">
             <CircularProgress color="success" className="py-48"/>
